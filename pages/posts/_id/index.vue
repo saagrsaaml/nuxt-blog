675204406f2bfc1b2064b0ba0cccc0b7112d25ea
vbox <saagrsaaml@gmail.com>
2018-07-03 14:43:51 +0545

    create post page and add styles in css

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="single-post-page">
+    <!-- main body content -->
+    <div class="main-content">
+      <div class="container">
+        <div class="columns is-centered is-multiline">
+          <div class="column is-6">
+            <div class="post">
+              <!-- post content -->
+              <div class="content">
+                <div class="content-header has-text-centered">
+                  <h3 class="title is-3"><b><a href="">Magicians are Crazy</a></b></h3>
+                  <h4 class="is-5 is-spaced">Some things are meant to suck the life out of you</h4>
+                  <h6 class="subtitle is-6">Posted by Author on April 7th 2017 at 9:33pm</h6>
+                  <hr/>
+                </div>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc purus elit, sagittis vitae volutpat a, egestas ut mi. Maecenas viverra lorem nec ultrices dictum. Fusce fringilla risus eget pretium suscipit. Sed malesuada ante ut faucibus gravida. Morbi tellus nisl, tincidunt sed fermentum eget, ullamcorper eu odio. Aliquam erat volutpat. Etiam tincidunt consequat nisi. Cras vulputate eros id nisl accumsan, nec pharetra quam vehicula. Aliquam in gravida diam. Aenean condimentum sapien vel velit pellentesque blandit nec vel dui. Quisque mattis massa in velit fringilla venenatis sit amet a nisi. Integer libero justo, dapibus in gravida at, posuere a lorem.</p>
+                <p>Vestibulum faucibus accumsan nisi vitae sagittis. Nam rhoncus vulputate malesuada. Morbi sodales posuere mauris, elementum fermentum nibh efficitur quis. Integer quis pretium massa, quis bibendum nulla. Sed in placerat orci, in interdum metus. Praesent varius lacus lacus, ac lacinia urna malesuada vitae. Ut porta tempor nisi, vitae pretium odio consectetur a. Suspendisse tempor purus vel ullamcorper rutrum. Morbi tellus metus, tempor ut enim in, accumsan tristique ex. Nulla congue lacus nisl. Aliquam porttitor orci sapien, id tincidunt tellus rhoncus et. Aliquam in elit lacus. Ut vitae lorem sapien. Proin erat ex, mattis quis ante non, semper tincidunt mi. </p>
+              </div>
+              <!-- end of post content -->
+            </div>
+            <!-- content nav -->
+            <nav class="has-text-right">
+              <a class="button" href="index.html">Back to Home</a>
+            </nav>
+            <!-- end of content nav -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end of main content -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="posts-page">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>

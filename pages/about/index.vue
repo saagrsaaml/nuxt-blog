d09494c59193b0f6bfe9177b564fc785295af3d6
vbox <saagrsaaml@gmail.com>
2018-07-03 14:18:55 +0545

    create about page

---
+++ pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about-page">
+    <p>
+      This is my about page
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>

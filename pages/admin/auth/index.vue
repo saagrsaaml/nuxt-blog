19b1296f8cabe874204e0c54bc341750098ba8c9
vbox <saagrsaaml@gmail.com>
2018-07-04 14:35:49 +0545

    create auth page

--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="auth">
-
+  <div class="auth-admin-page">
+    <div class="auth-container">
+      <form>
+        <AppControlInput type="email">E-Mail Address</AppControlInput>
+        <AppControlInput type="password">Password</AppControlInput>
+        <AppButton type="submit">{{ isLogin ? 'Login' : 'Sign Up' }}</AppButton>
+        <AppButton type="button" @click="isLogin =! isLogin">Switch to {{ isLogin ? 'Sign Up' : 'Login' }}</AppButton>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+import AppControlInput from "~/components/UI/AppControlInput";
+import AppButton from '~/components/UI/AppButton'
 export default {
-  name: "index"
+  name: "index",
+  components: {AppControlInput, AppButton},
+  data() {
+    return {
+      isLogin: true
+    }
+  }
 }
 </script>
 

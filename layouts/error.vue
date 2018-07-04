2e9da5577645880462d82f2e0b879cef71ee93a3
vbox <saagrsaaml@gmail.com>
2018-07-04 15:02:06 +0545

    refactor auth UI

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="error-page">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "error"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -1,12 +1,32 @@
 <template>
   <div class="auth-admin-page">
     <div class="auth-container">
-      <form>
-        <AppControlInput type="email">E-Mail Address</AppControlInput>
-        <AppControlInput type="password">Password</AppControlInput>
-        <AppButton type="submit">{{ isLogin ? 'Login' : 'Sign Up' }}</AppButton>
-        <AppButton type="button" @click="isLogin =! isLogin">Switch to {{ isLogin ? 'Sign Up' : 'Login' }}</AppButton>
-      </form>
+      <section class="hero is-fullheight">
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <div class="column is-4 is-offset-4">
+              <h3 class="title has-text-grey">Login</h3>
+              <p class="subtitle has-text-grey">Please login to proceed.</p>
+              <div class="box">
+                <figure class="avatar">
+                  <img src="https://placehold.it/128x128">
+                </figure>
+                <form>
+                  <AppControlInput type="email">E-Mail Address</AppControlInput>
+                  <AppControlInput type="password">Password</AppControlInput>
+                  <AppButton type="submit">{{ isLogin ? 'Login' : 'Sign Up' }}</AppButton>
+                  <AppButton type="button" @click="isLogin =! isLogin">Switch to {{ isLogin ? 'Sign Up' : 'Login' }}</AppButton>
+                </form>
+              </div>
+              <p class="has-text-grey">
+                <a href="../">Sign Up</a> &nbsp;·&nbsp;
+                <a href="../">Forgot Password</a> &nbsp;·&nbsp;
+                <a href="../">Need Help?</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   </div>
 </template>

c54cf04af8aa7a1b6c735048c0a915ebc5afdfbd
vbox <saagrsaaml@gmail.com>
2018-07-04 15:42:23 +0545

    use asyncData to load data from server side

--- pages/index.vue
+++ pages/index.vue
@@ -31,34 +31,44 @@ import PostList from "~/components/Posts/PostList";
 
 export default {
   name: 'index',
-  data() {
-    return {
-       loadedPosts: [
-         {
-           id: 1,
-           thumbnail: "https://thumbs.dreamstime.com/b/woman-wearing-yellow-floral-top-116695890.jpg",
-           title: "Old People Work Hard",
-           previewText: "At least I believe they do"
-         },
-         {
-           id: 2,
-           thumbnail: "https://thumbs.dreamstime.com/b/green-white-red-abstract-painting-116695780.jpg",
-           title: "Crawling Over Sand",
-           previewText: "ricks to crawl correctly!"
-         },
-         {
-           id: 3,
-           thumbnail: "https://thumbs.dreamstime.com/b/woman-wearing-yellow-floral-top-116695890.jpg",
-           title: "Apples Are Real",
-           previewText: "Apples are born from apples"
-         }
-       ]
-    }
+  // data() {
+  //   return {
+  //      loadedPosts: []
+  //   }
+  // },
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: 1,
+            thumbnail: "https://thumbs.dreamstime.com/b/woman-wearing-yellow-floral-top-116695890.jpg",
+            title: "Old People Work Hard",
+            previewText: "At least I believe they do"
+          },
+          {
+            id: 2,
+            thumbnail: "https://thumbs.dreamstime.com/b/green-white-red-abstract-painting-116695780.jpg",
+            title: "Crawling Over Sand",
+            previewText: "ricks to crawl correctly!"
+          },
+          {
+            id: 3,
+            thumbnail: "https://thumbs.dreamstime.com/b/woman-wearing-yellow-floral-top-116695890.jpg",
+            title: "Apples Are Real",
+            previewText: "Apples are born from apples"
+          }
+        ]
+      })
+    }, 1500)
   },
   components: {
     PostList,
     Logo,
     PostPreview
+  },
+  created () {
+
   }
 }
 </script>

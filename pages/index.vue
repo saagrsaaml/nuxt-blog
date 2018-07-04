10440e0478a9e70a84fca4a1317a4d733b2fbcf5
vbox <saagrsaaml@gmail.com>
2018-07-04 16:41:43 +0545

    reject promise and show our error layout

--- pages/index.vue
+++ pages/index.vue
@@ -38,30 +38,31 @@ export default {
   // },
   asyncData(context) {
     return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          loadedPosts: [
-            {
-              id: '1',
-              thumbnail: "https://thumbs.dreamstime.com/b/woman-wearing-yellow-floral-top-116695890.jpg",
-              title: "Old People Work Hard",
-              previewText: "At least I believe they do"
-            },
-            {
-              id: '2',
-              thumbnail: "https://thumbs.dreamstime.com/b/green-white-red-abstract-painting-116695780.jpg",
-              title: "Crawling Over Sand",
-              previewText: "ricks to crawl correctly!"
-            },
-            {
-              id: '3',
-              thumbnail: "https://thumbs.dreamstime.com/b/woman-wearing-yellow-floral-top-116695890.jpg",
-              title: "Apples Are Real",
-              previewText: "Apples are born from apples"
-            }
-          ]
-        })
-      }, 1500)
+      // setTimeout(() => {
+      //   resolve({
+      //     loadedPosts: [
+      //       {
+      //         id: '1',
+      //         thumbnail: "https://thumbs.dreamstime.com/b/woman-wearing-yellow-floral-top-116695890.jpg",
+      //         title: "Old People Work Hard",
+      //         previewText: "At least I believe they do"
+      //       },
+      //       {
+      //         id: '2',
+      //         thumbnail: "https://thumbs.dreamstime.com/b/green-white-red-abstract-painting-116695780.jpg",
+      //         title: "Crawling Over Sand",
+      //         previewText: "ricks to crawl correctly!"
+      //       },
+      //       {
+      //         id: '3',
+      //         thumbnail: "https://thumbs.dreamstime.com/b/woman-wearing-yellow-floral-top-116695890.jpg",
+      //         title: "Apples Are Real",
+      //         previewText: "Apples are born from apples"
+      //       }
+      //     ]
+      //   })
+      // }, 1500)
+      reject(new Error())
     }).then(data => {
       return data
     }).catch(e => {

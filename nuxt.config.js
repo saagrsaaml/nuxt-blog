e211f20a627d24f372098fcd271a12a8f623d2ae
vbox <saagrsaaml@gmail.com>
2018-07-08 17:47:45 +0545

    creating global date filter

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,8 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '~plugins/core-components.js'
+    '~plugins/core-components.js',
+    '~plugins/date-filter.js'
   ],
 
   /*
--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -11,7 +11,7 @@
                 <div class="content-header has-text-centered">
                   <h3 class="title is-3"><b><a href="">{{ loadedPost.title }}</a></b></h3>
                   <h4 class="is-5 is-spaced">Some things are meant to suck the life out of you</h4>
-                  <h6 class="subtitle is-6">Posted by {{ loadedPost.author }} on {{ loadedPost.updatedDate }}</h6>
+                  <h6 class="subtitle is-6">Posted by {{ loadedPost.author }} on {{ loadedPost.updatedDate | date }}</h6>
                   <hr/>
                 </div>
                 <p>{{ loadedPost.content }}</p>

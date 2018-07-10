3b1c710b6f767e9cd145e7346d1f19dca3bdf62d
vbox <saagrsaaml@gmail.com>
2018-07-10 14:15:31 +0545

    storing the token id

--- store/index.js
+++ store/index.js
@@ -3,7 +3,8 @@ import Vuex from 'vuex'
 const createStore = () =>{
   return new Vuex.Store({
     state: {
-      loadedPosts: []
+      loadedPosts: [],
+      token: null
     },
     mutations: {
       setPosts(state, posts) {
@@ -17,6 +18,9 @@ const createStore = () =>{
           return post.id === editedPost.id
         })
         state.loadedPosts[postIndex] = editedPost
+      },
+      setToken(state, token) {
+        state.token = token
       }
     },
     actions: {
@@ -46,6 +50,19 @@ const createStore = () =>{
             vuexContext.commit('editPost', editedPost)
           })
           .catch(e => console.log(e))
+      },
+      authenticateUser(vuexContext, authData) {
+        let authUrl = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key='+ process.env.fbAPIKey
+        if(!authData.isLogin){
+          authUrl = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + process.env.fbAPIKey
+        }
+        return this.$axios.$post(authUrl,{
+          email: authData.email,
+          password: authData.password,
+          returnSecureToken: true
+        }).then(result => {
+          vuexContext.commit('setToken', result.idToken)
+        }).catch(e => console.error(e))
       }
     },
     getters: {

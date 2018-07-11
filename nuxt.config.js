c4df8f34da17763366ac98e2950e27ce0851af69
vbox <saagrsaaml@gmail.com>
2018-07-11 13:50:10 +0545

    add serverside middleware and install express and body-parser

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 const pkg = require('./package')
+const bodyParser = require('./body-parser')
 
 module.exports = {
   mode: 'universal',
@@ -98,5 +99,9 @@ module.exports = {
   transition: {
     name: 'fade',
     mode: 'out-in'
-  }
+  },
+  serverMiddleware: [
+    bodyParser.json(),
+    '~/api'
+  ]
 }
